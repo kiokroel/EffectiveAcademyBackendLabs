599d104f3261bf6de4a9b6066167fd3467ffcfa6
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-11-07 19:47:42 +0600

    style: code style fixes

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,27 +12,23 @@ class BooksInline(admin.TabularInline):
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
 
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    fields = ["first_name", "last_name", ("date_of_birth", "date_of_death")]
 
     inlines = [BooksInline]
 
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_filter = ('status', 'due_back')
+    list_filter = ("status", "due_back")
 
     fieldsets = (
-        (None, {
-            'fields': ('book', 'imprint', 'id')
-        }),
-        ('Availability', {
-            'fields': ('status', 'due_back')
-        }),
+        (None, {"fields": ("book", "imprint", "id")}),
+        ("Availability", {"fields": ("status", "due_back")}),
     )
 
-    list_display = ('book', 'status', 'due_back', 'id')
+    list_display = ("book", "status", "due_back", "id")
 
 
 class BooksInstanceInline(admin.TabularInline):
@@ -41,6 +37,6 @@ class BooksInstanceInline(admin.TabularInline):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ("title", "author", "display_genre")
 
     inlines = [BooksInstanceInline]
