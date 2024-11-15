06077abb12b0f00ebeafaff3046008aa22541599
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-11-15 22:10:29 +0600

    chore: added display of the number of visits to the main page

--- catalog/views.py
+++ catalog/views.py
@@ -4,23 +4,34 @@ from django.shortcuts import render
 
 from .models import Book, Author, BookInstance, Genre
 
+
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
     """
     # Генерация "количеств" некоторых главных объектов
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        "index.html",
+        context={
+            "num_books": num_books,
+            "num_instances": num_instances,
+            "num_instances_available": num_instances_available,
+            "num_authors": num_authors,
+            "num_visits": num_visits,
+        },
     )
 
 
